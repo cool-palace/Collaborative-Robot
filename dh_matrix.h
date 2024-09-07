ca4d44de3d7931a256beb453533a51ef5ebf66dc
Cool Palace <wynaut@yandex.ru>
2024-09-07 14:36:03 +0300

    Added point class and coordinates getter function

--- dh_matrix.h
+++ dh_matrix.h
@@ -3,6 +3,17 @@
 #include <QtMath>
 #include <QDebug>
 
+class Point {
+private:
+    double x_, y_, z_;
+
+public:
+    Point(double x = 0, double y = 0, double z = 0) : x_(x), y_(y), z_(z) {}
+    double x() const { return x_; }
+    double y() const { return y_; }
+    double z() const { return z_; }
+};
+
 class DH_Matrix
 {
 private:
@@ -11,6 +22,7 @@ private:
 public:
     DH_Matrix(int theta = 0, double a = 0, double d = 0, int alpha = 0);
     DH_Matrix operator*(const DH_Matrix& other) const;
+    Point coordinates() const { return Point(matrix[0][3], matrix[1][3], matrix[2][3]); }
     friend QDebug& operator<<(QDebug debug, const DH_Matrix& m);
 };
 
