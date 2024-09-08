26c179b9114f13e073badcd0131c8bce15b33c90
Cool Palace <wynaut@yandex.ru>
2024-09-08 11:45:23 +0300

    Reorganized Scene class members

--- scene.cpp
+++ scene.cpp
@@ -2,25 +2,10 @@
 
 Scene::Scene(QObject* parent) : QGraphicsScene(parent)
     , line_pen(QBrush(Qt::green),3)
-    , x_axis(new QGraphicsLineItem())
-    , z_axis(new QGraphicsLineItem())
-    , x_label(new QGraphicsTextItem("x"))
-    , z_label(new QGraphicsTextItem("z"))
-    , origin_label(new QGraphicsTextItem("0"))
-    , x_1_mark(new QGraphicsLineItem())
-    , x_minus_1_mark(new QGraphicsLineItem())
-    , z_1_mark(new QGraphicsLineItem())
-    , x_1_label(new QGraphicsTextItem("1"))
-    , x_minus_1_label(new QGraphicsTextItem("-1"))
-    , z_1_label(new QGraphicsTextItem("1")) { }
+{ }
 
 Scene::~Scene() {
-    clear();
-    delete x_axis;
-    delete z_axis;
-    delete x_label;
-    delete z_label;
-    delete origin_label;
+    clear_all();
 }
 
 void Scene::init() {
@@ -29,50 +14,98 @@ void Scene::init() {
     double z_axis_length = height();
     scale = z_axis_length / 2;
 
-    // Drawing reference plane in XOZ plane
-    z_axis->setLine(z_axis_pos, 0, z_axis_pos, z_axis_length + margin);
+    // Setting X axis
+    auto x_axis = new QGraphicsLineItem();
+    axes.append(x_axis);
     x_axis->setLine(0, z_axis_length, x_axis_length, z_axis_length);
+
+    // Setting Z axis
+    auto z_axis = new QGraphicsLineItem();
+    axes.append(z_axis);
+    z_axis->setLine(z_axis_pos, 0, z_axis_pos, z_axis_length + margin);
+
+    // Adding axes
+    QPen axis_pen(QBrush(Qt::black),2,Qt::DashDotLine);
+    for (auto axis : axes) {
+        axis->setPen(axis_pen);
+        addItem(axis);
+    }
+
+    // Setting marks
+    auto x_1_mark = new QGraphicsLineItem();
+    marks.append(x_1_mark);
+    x_1_mark->setLine(z_axis_pos + scale, z_axis_length - margin/2, z_axis_pos + scale, z_axis_length + margin/2);
+
+    auto x_minus_1_mark = new QGraphicsLineItem();
+    marks.append(x_minus_1_mark);
+    x_minus_1_mark->setLine(z_axis_pos - scale, z_axis_length - margin/2, z_axis_pos - scale, z_axis_length + margin/2);
+
+    auto z_1_mark = new QGraphicsLineItem();
+    marks.append(z_1_mark);
+    z_1_mark->setLine(z_axis_pos - margin/2, z_axis_length - scale, z_axis_pos + margin/2, z_axis_length - scale);
+
+    // Adding marks
+    for (auto mark : marks) {
+        addItem(mark);
+    }
+
+    // Setting labels
+    auto x_label = new QGraphicsTextItem("x");
+    labels.append(x_label);
     x_label->setPos(x_axis->line().p2() + x_axis_label_offset);
+
+    auto z_label = new QGraphicsTextItem("z");
+    labels.append(z_label);
     z_label->setPos(z_axis->line().p1() + z_axis_label_offset);
+
+    auto origin_label = new QGraphicsTextItem("0");
+    labels.append(origin_label);
     origin_label->setPos(z_axis->line().p2() + QPointF(5,-10));
 
-    x_1_mark->setLine(z_axis_pos + scale, z_axis_length - margin/2, z_axis_pos + scale, z_axis_length + margin/2);
-    x_minus_1_mark->setLine(z_axis_pos - scale, z_axis_length - margin/2, z_axis_pos - scale, z_axis_length + margin/2);
-    z_1_mark->setLine(z_axis_pos - margin/2, z_axis_length - scale, z_axis_pos + margin/2, z_axis_length - scale);
+    auto x_1_label = new QGraphicsTextItem("1");
+    labels.append(x_1_label);
     x_1_label->setPos(x_1_mark->line().p2() + x_axis_label_offset);
+
+    auto x_minus_1_label = new QGraphicsTextItem("-1");
+    labels.append(x_minus_1_label);
     x_minus_1_label->setPos(x_minus_1_mark->line().p2() + x_axis_label_offset);
+
+    auto z_1_label = new QGraphicsTextItem("1");
+    labels.append(z_1_label);
     z_1_label->setPos(z_1_mark->line().p2() + QPointF(5,-10));
 
-    QPen axis_pen(QBrush(Qt::black),2,Qt::DashDotLine);
-    x_axis->setPen(axis_pen);
-    z_axis->setPen(axis_pen);
-
-    addItem(x_axis);
-    addItem(z_axis);
-    addItem(x_label);
-    addItem(z_label);
-    addItem(origin_label);
-    addItem(x_1_mark);
-    addItem(x_minus_1_mark);
-    addItem(z_1_mark);
-    addItem(x_1_label);
-    addItem(x_minus_1_label);
-    addItem(z_1_label);
+    // Adding labels
+    for (auto label : labels) {
+        addItem(label);
+    }
 }
 
-void Scene::clear() {
+void Scene::clear_lines() {
     for (auto line : lines) {
         delete line;
     }
     lines.clear();
 }
 
+void Scene::clear_all() {
+    clear_lines();
+    for (auto axis : axes) {
+        delete axis;
+    }
+    for (auto mark : marks) {
+        delete mark;
+    }
+    for (auto label : labels) {
+        delete label;
+    }
+}
+
 void Scene::draw_lines(const QList<Point> & points, int rotation_angle) {
-    clear();
-    double theta = qDegreesToRadians(static_cast<double>(rotation_angle));
+    clear_lines();
+    double phi = qDegreesToRadians(static_cast<double>(rotation_angle));
     for (int i = 0; i < points.size() - 1; ++i) {
-        QLineF line = QLineF((points[i].x()*qCos(theta) - points[i].y()*qSin(theta)) * scale + width()/2, height() - points[i].z() * scale,
-                             (points[i+1].x()*qCos(theta) - points[i+1].y()*qSin(theta)) * scale + width()/2, height() - points[i+1].z() * scale);
+        QLineF line = QLineF((points[i].x()*qCos(phi) - points[i].y()*qSin(phi)) * scale + width()/2, height() - points[i].z() * scale,
+                             (points[i+1].x()*qCos(phi) - points[i+1].y()*qSin(phi)) * scale + width()/2, height() - points[i+1].z() * scale);
         lines.push_back(new QGraphicsLineItem(line));
         lines.back()->setPen(line_pen);
         addItem(lines.back());
--- scene.h
+++ scene.h
@@ -17,24 +17,17 @@ public:
     Scene(QObject* parent = nullptr);
     virtual ~Scene();
     void init();
-    void clear();
     void draw_lines(const QList<Point>&, int rotation_angle = 0);
-    void set_yoz(bool enable) { x_label->setPlainText(enable ? "y" : "x"); }
+    void set_yoz(bool enable) { labels.first()->setPlainText(enable ? "y" : "x"); }
 
 private:
+    void clear_lines();
+    void clear_all();
     double scale;
     QPen line_pen;
-    QGraphicsLineItem * x_axis;
-    QGraphicsLineItem * z_axis;
-    QGraphicsTextItem * x_label;
-    QGraphicsTextItem * z_label;
-    QGraphicsLineItem * x_1_mark;
-    QGraphicsLineItem * x_minus_1_mark;
-    QGraphicsLineItem * z_1_mark;
-    QGraphicsTextItem * x_1_label;
-    QGraphicsTextItem * x_minus_1_label;
-    QGraphicsTextItem * z_1_label;
-    QGraphicsTextItem * origin_label;
+    QList<QGraphicsLineItem*> axes;
+    QList<QGraphicsLineItem*> marks;
+    QList<QGraphicsTextItem*> labels;
     QList<QGraphicsLineItem*> lines;
 };
 
